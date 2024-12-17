44aba17d6600879395550b953509af1823e0d768
Zepei Sun <zepeisun@dhcp-10-105-60-85.wireless.northwestern.private>
2024-12-17 09:21:42 -0600

    add utility

--- utility.py
+++ utility.py
@@ -57,13 +57,15 @@ def select_channel_to_attack(resource_map,num_subchannels):
     return np.random.choice(busy_indices) if len(busy_indices) > 0 else np.random.choice(num_subchannels)
 
 
-def update_neighbors(vehicle, subchannel, vehicles_info,subframe_position):
+def update_neighbors(vehicle, subchannel, vehicles_info,subframe_position,attackrs_info,attacker_start_index):
     """
     Inform the neighbors of the vehicle's subchannel choice and update their resource maps.
     """
     for neighbor in vehicles_info[vehicle]['neighbors']:
-        vehicles_info[neighbor]['resource_map'][subchannel, subframe_position] = 1  # Mark usage
-
+        if neighbor < attacker_start_index:
+            vehicles_info[neighbor]['resource_map'][subchannel, subframe_position] = 1  # Mark usage
+        else:
+            attackrs_info[neighbor]['resource_map'][subchannel, subframe_position] = 1
 
 def update_neighbors_row(vehicle_info,channel_pick,subframe_position,attackrs_info,attacker_start_index):
     for vehicle, subchannel in channel_pick.items():
@@ -80,28 +82,39 @@ def update_neighbors_row(vehicle_info,channel_pick,subframe_position,attackrs_in
                 else:
                     attackrs_info[neighbor]['resource_map'][subchannel, subframe_position] = 1
 
-def package_received(attempt_transmission,successful_transmissions,station_info):
+
+def package_received(attempt_transmission,station_info,attacker_start_index,attackers_info):
+    successful_transmissions = {}
     for channel, vehicles in attempt_transmission.items():
         if  len(vehicles) == 1:
-            vehicle = vehicles[0]
-            successful_transmissions[vehicle] = station_info[vehicle]['neighbors']
+            if vehicles[0] < attacker_start_index:
+                vehicle = vehicles[0]
+                successful_transmissions[vehicle] = station_info[vehicle]['neighbors']
+            else:
+                pass
         else:     
             all_neighbors = {}
             for vehicle in vehicles:
-                all_neighbors[vehicle] = station_info[vehicle]['neighbors']
+                if vehicle < attacker_start_index:
+                    all_neighbors[vehicle] = station_info[vehicle]['neighbors']
+                else:
+                    all_neighbors[vehicle] = attackers_info[vehicle]['neighbors']
 
             sets = {key: set(value) for key, value in all_neighbors.items()}
 
             # Find overlapping part (intersection of all sets)
             exclusive_neighbors = {}
             for vehicle, neighbor_set in sets.items():
+                if vehicle < attacker_start_index:
                     # Union of neighbors of all other vehicles
-                others_union = set().union(*(sets[other] for
-                                 other in sets.keys() if other != vehicle))
-                # Exclusive neighbors for the current vehicle
-                unique_neighbors = neighbor_set - others_union
-                exclusive_neighbors[vehicle] = list(unique_neighbors)
-                successful_transmissions.update(exclusive_neighbors)
+                    others_union = set().union(*(sets[other] for
+                                    other in sets.keys() if other != vehicle))
+                    # Exclusive neighbors for the current vehicle
+                    unique_neighbors = neighbor_set - others_union
+                    exclusive_neighbors[vehicle] = list(unique_neighbors)
+                    successful_transmissions.update(exclusive_neighbors)
+                else:
+                    pass
     for key, values in successful_transmissions.items():
         if key in values:
             values.remove(key)  # This removes the vehicle from its neighbor list in place
@@ -207,7 +220,7 @@ def calculate_ipg_tail(data_list):
 
     cdf = np.cumsum(counts)/len(ipg_sorted)
     ccdf = 1 - cdf
-    target_ccdf = 10 ** -4
+    target_ccdf = 10 ** -5
     interpolator = interp1d(ccdf, unique_value, fill_value="extrapolate")
     x_value_at_target_ccdf = interpolator(target_ccdf)
     print(f"X-axis value at CCDF = 10^-5 : {x_value_at_target_ccdf}")
